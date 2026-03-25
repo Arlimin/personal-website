54bd2a802a0c0ab89f1e2d0e364e4408a03bf883
Your Name <you@example.com>
2026-03-24 21:41:58 -0400

    Fix routing and layout structure

---
+++ app/personal-site/layout.tsx
@@ -0,0 +1,16 @@
+import Header from '../personal-site/components/Header';
+import Footer from '../personal-site/components/footer';
+
+export default function SiteLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+}
