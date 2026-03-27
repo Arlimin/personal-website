1858239f5bd60868616f52a21878c41c9a6cc84a
Your Name <you@example.com>
2026-03-26 21:16:26 -0400

    Maybe it worked

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,12 +8,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>
+    
+<html lang="en">
+      <body className="bg-transparent text-neutral-300">
         <Header />
         <main>{children}</main>
         <Footer />
       </body>
     </html>
+
   );
 }
